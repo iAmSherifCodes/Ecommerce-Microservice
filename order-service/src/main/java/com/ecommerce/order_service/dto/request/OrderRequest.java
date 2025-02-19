31dac252584f454366cc095239a965108e1900e4
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-02-19 10:57:58 +0100

    Overloaded OrderRequest Constructor

--- order-service/src/main/java/com/ecommerce/order_service/dto/request/OrderRequest.java
+++ order-service/src/main/java/com/ecommerce/order_service/dto/request/OrderRequest.java
@@ -1,11 +1,12 @@
 package com.ecommerce.order_service.dto.request;
 
 import com.ecommerce.order_service.model.OrderStatus;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor
 public class OrderRequest {
     private String productId;
     private Integer quantity;
