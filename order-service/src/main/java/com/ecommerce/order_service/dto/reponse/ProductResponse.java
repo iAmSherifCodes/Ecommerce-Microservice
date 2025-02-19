8ace7132f4592228de68c09b370ee7683bd42045
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-02-19 10:57:30 +0100

    Overloaded ProductResponse Constructor

--- order-service/src/main/java/com/ecommerce/order_service/dto/reponse/ProductResponse.java
+++ order-service/src/main/java/com/ecommerce/order_service/dto/reponse/ProductResponse.java
@@ -1,12 +1,13 @@
 package com.ecommerce.order_service.dto.reponse;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
-@Getter @Setter @Builder
+@Getter @Setter @Builder @AllArgsConstructor
 public class ProductResponse {
     private String id;
     private String name;
