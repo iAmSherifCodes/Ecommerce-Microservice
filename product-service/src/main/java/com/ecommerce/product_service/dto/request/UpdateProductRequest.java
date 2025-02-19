d40d8bcd71427a75fa8dfdee6739c9b27e532d2c
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-02-19 10:10:03 +0100

    Modified Argument Constructor in DTO

--- product-service/src/main/java/com/ecommerce/product_service/dto/request/UpdateProductRequest.java
+++ product-service/src/main/java/com/ecommerce/product_service/dto/request/UpdateProductRequest.java
@@ -1,15 +1,16 @@
 package com.ecommerce.product_service.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
-@Getter @Setter
+@Getter @Setter @AllArgsConstructor
 public class UpdateProductRequest {
     private String id;
     private String name;
-    private String description;
     private BigDecimal price;
+    private String description;
 }
