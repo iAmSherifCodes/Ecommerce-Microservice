234a3bfa5cf5b632b0ded4beb079becf24301f7b
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-02-19 03:48:23 +0100

    Product-service: Implemented GlobalExceptionHandler

--- product-service/src/main/java/com/ecommerce/product_service/Exception/GlobalExceptionHandler.java
+++ product-service/src/main/java/com/ecommerce/product_service/Exception/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.ecommerce.product_service.Exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleProductNotFoundException(ProductNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "Product Not Found");
+        response.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
 }
