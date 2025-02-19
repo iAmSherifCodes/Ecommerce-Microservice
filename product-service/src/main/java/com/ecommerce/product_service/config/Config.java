498794afe6984d16d83a99ea8e73887aaba4e503
Sherifdeen Awofiranye <awofiranyesherif4@gmail.com>
2025-02-19 03:48:50 +0100

    Product-service: Implemented Config file

--- product-service/src/main/java/com/ecommerce/product_service/config/Config.java
+++ product-service/src/main/java/com/ecommerce/product_service/config/Config.java
@@ -1,4 +1,8 @@
-package com.ecommerce.product_service.config;
-
-public class Config {
-}
+//package com.ecommerce.product_service.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class Config {
+//}
